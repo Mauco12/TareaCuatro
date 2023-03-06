4974dffaa2b0696140fa40f8ee983b796b3b7c08
Mauricio <mauriciorodriguez90@aragon.unam.mx>
2023-03-05 23:38:20 -0600

    Initial commit

---
+++ src/Gato.java
@@ -0,0 +1,22 @@
+public class Gato {
+
+    String nombreG;
+    String colorG;
+    int edadG;
+    String razaG;
+    boolean vivoG;
+     public Gato(String nombreG, String colorG, int edadG,String razaG, boolean vivoG){
+         this.nombreG = nombreG;
+         this.colorG = colorG;
+         this.edadG = edadG;
+         this.razaG = razaG;
+         this.vivoG = vivoG;
+
+
+     }
+
+
+}
+
+
+
---
+++ src/Perro.java
@@ -0,0 +1,20 @@
+public class Perro {
+    String nombreP;
+    String tipoComidaP;
+    int paseosP;
+    String razaP;
+    boolean vivoP;
+
+    public Perro(String nombreP, String tipoComidaP, int paseosP,String razaP, boolean vivoP){
+        this.nombreP = nombreP;
+        this.tipoComidaP = tipoComidaP;
+        this.paseosP = paseosP;
+        this.razaP = razaP;
+        this.vivoP = vivoP;
+
+
+    }
+
+
+}
+
