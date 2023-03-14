47393ad0fb44cf88c152321cc0dbbadcff5167e8
Mauricio <mauriciorodriguez90@aragon.unam.mx>
2023-03-13 18:26:49 -0600

    Tarea

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main (String[] args) {
+
+      System.out.println("Primera clase de 'Gato'\n".toUpperCase());
+      Gato gatoUno = new Gato("Michi","Gris", 5,"Raza gato", true);
+      System.out.println("El gato se llama: " + gatoUno.nombreG + "\nColor: " + gatoUno.colorG + "\nSu edad es: " + gatoUno.edadG + "\nEs raza: " + gatoUno.razaG + "\n¿Esta vivo? " + gatoUno.vivoG);
+
+      System.out.println("\nSegunda clase de 'Perro'\n".toUpperCase());
+      Perro perroUno = new Perro("Firulais","La cara",4,"Pitbull",true);
+      System.out.println("El perro se llama: " + perroUno.nombreP + "\nTipo de comida: " + perroUno.tipoComidaP + "\nPaseos al dia: " + perroUno.paseosP + "\nSu raza es: " + perroUno.razaP + "\n¿Esta vivo? " + perroUno.vivoP);
+
+
     }
+
+
+
+
 }
+
